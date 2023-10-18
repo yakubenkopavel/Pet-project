6a0f8e25664eb6a8f454bd5dfe828af929ea28e5
Yakubenko Pavel <yakubenkopavel@bk.ru>
2023-10-18 16:38:52 +0300

    added template using thymeleaf

--- src/main/java/com/Airline/Controller/HomeController.java
+++ src/main/java/com/Airline/Controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/welcome")
     public String home(){
         return "home";
     }
