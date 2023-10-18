5c53ca7721ba5ec79930ec6c2bd6ae0c65b8d2e1
Yakubenko Pavel <yakubenkopavel@bk.ru>
2023-10-18 18:51:59 +0300

    changed the homeController

--- src/main/java/com/Airline/Controller/HomeController.java
+++ src/main/java/com/Airline/Controller/HomeController.java
@@ -1,12 +1,14 @@
-package com.Airline;
+package com.Airline.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
-    @GetMapping("/welcome")
+    @GetMapping("/")
     public String home(){
-        return "home";
+        return "welcome";
     }
 }
