77969755347e982fa6558b6aaacf1023f973638d
Yakubenko Pavel <yakubenkopavel@bk.ru>
2024-02-12 13:50:16 +0300

    deleted the target

--- src/main/java/com/Surface/Controllers/HomeController.java
+++ src/main/java/com/Surface/Controllers/HomeController.java
@@ -4,10 +4,11 @@ import com.Surface.Models.FileViewer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
 
 import java.io.File;
 import java.util.ArrayList;
